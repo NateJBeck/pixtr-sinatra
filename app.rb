336f236ca38f1f6c56f3048191ed3e2f844f0393
Nathan Beck <nathan.beck10@gmail.com>
2014-09-24 15:58:55 -0400

    using .where and .find on images and gallery

--- app.rb
+++ app.rb
@@ -2,47 +2,71 @@ require "sinatra"
 if development?
   require "sinatra/reloader"
 end
-require "pg"
 
-database = PG.connect({ dbname: "photo_gallery" })
-#sets database to be already made photo_gallery, using PG.connect
-#
-get "/" do
-  galleries = database.exec_params("SELECT name FROM galleries")
-  #@gallery_names = []
- 
-  @gallery_names = galleries.map { |gallery| gallery["name"]}
+require "active_record"
 
-  #galleries.each do |gallery|
-  #  @gallery_names << gallery["name"]
-  #end
-  
- # @galleries = GALLERIES
- # ake galleries a database.exec put names into gallery array
- #  use in home view
+ActiveRecord::Base.establish_connection(
+  adapter: "postgresql",
+  database: "photo_gallery"
+)
 
-  erb :home #sinatra knows that method ERB calling :home will go to VIEWS/home.erb
+class Gallery < ActiveRecord::Base
 end
 
-#get "/galleries/:name" do #:name here is part of a string, it is not a symbol
-#
+class Image < ActiveRecord::Base
+end
+
+get "/" do 
+  @galleries = Gallery.all
+  #@gallery_names = galleries.map {|gallery| gallery.name}
+  erb :home
+end
+
 get "/galleries/:id" do   
   id = params[:id]   #setting id to parameter input in URL   galleries/1   id = 1
+  
+  gallery = Gallery.find(id) #finds galleries based on ID
  
-  name_result = database.exec_params("SELECT name FROM galleries WHERE id = $1", [id])
-  #SELECT returns an array-like object from PG including names from galleries
-  #with id = 1  
-  
-  @name = name_result.first["name"]
-  #save @name as an instance variable to be available in :gallery view, equal to
-  #the value of the galleries "name" (value = "Cats", "Dogs" etc)
-  
-  @images = []
-  #@images = database.exec_params("SELECT * FROM images WHERE id = $1", [id])
-  
+  @name = gallery.name
+  @images = Image.where(gallery_id: id)   # use with second part in gallery
+ 
   erb :gallery
 end
 
+  #name_result = database.exec_params("SELECT name FROM galleries WHERE id = $1", [id])
+  #SELECT returns an array-like object from PG including names from galleries
+  #with id = 1  
+  
+#  @name = name_result.first["name"]
+  #save @name as an instance variable to be available in :gallery view, equal to
+  #the value of the galleries "name" (value = "Cats", "Dogs" etc)
+  
+ # @images = []
+  #@images = database.exec_params("SELECT * FROM images WHERE id = $1", [id])
+
+
+#database = PG.connect({ dbname: "photo_gallery" })
+#sets database to be already made photo_gallery, using PG.connecta
+
+#get "/" do
+#  galleries = database.exec_params("SELECT name FROM galleries")
+  #@gallery_names = []
+ 
+#  @gallery_names = galleries.map { |gallery| gallery["name"]}
+
+      #galleries.each do |gallery|
+      #  @gallery_names << gallery["name"]
+      #end
+      
+      # @galleries = GALLERIES
+      # ake galleries a database.exec put names into gallery array
+      #  use in home view
+
+#  erb :home           #sinatra knows that method ERB calling :home will go to VIEWS/home.erb
+#end
+
+#get "/galleries/:name" do #:name here is part of a string, it is not a symbol
+#
 
 #et "/projects/:id" do
 #  pass project id as variable
@@ -50,8 +74,7 @@ end
 #end
 
 #views/layout.erb
-
-
+#
   # redirec(to("/bar"), 301)
   # [200, { "Location" => "/bar" } ""]
   # [200, "Body"]
