0dbb92dd2f53b8aba525e38d3b7835e568138bd7
Nathan Beck <nathan.beck10@gmail.com>
2014-09-24 11:08:49 -0400

    Started using database

--- app.rb
+++ app.rb
@@ -2,21 +2,32 @@ require "sinatra"
 if development?
   require "sinatra/reloader"
 end
+require "pg"
 
-GALLERIES = {
-  "cats" => ["colonel_meow.jpg", "grumpy_cat.png"],
-  "dogs" => ["shibe.png"]
-}
-
+database = PG.connect({ dbname: "photo_gallery" })
+#sets database to be photo_gallery, using PG.connect
+#
 get "/" do
-  @galleries = GALLERIES
+  galleries = database.exec_params("SELECT name FROM galleries")
+  #@gallery_names = []
+ 
+  @gallery_names = galleries.map { |gallery| gallery["name"]}
+
+  #galleries.each do |gallery|
+  #  @gallery_names << gallery["name"]
+  #end
+  
+ # @galleries = GALLERIES
+ # ake galleries a database.exec put names into gallery array
+ #  use in home view
+
   erb :home #sinatra knows that method ERB calling :home will go to VIEWS/home.erb
 end
 
 get "/galleries/:name" do #:name here is part of a string, it is not a symbol
- @name = params[:name]  
- @images = GALLERIES[@name]
- erb :gallery
+  @name = params[:name]  
+  @images = GALLERIES[@name]
+  erb :gallery
 end
 #
 #
