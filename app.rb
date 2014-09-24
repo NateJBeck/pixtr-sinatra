040a4bf455fa536f3a9d469bf7157b6bf752788d
Nathan Beck <nathan.beck10@gmail.com>
2014-09-24 14:16:19 -0400

    pull gallery name

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ end
 require "pg"
 
 database = PG.connect({ dbname: "photo_gallery" })
-#sets database to be photo_gallery, using PG.connect
+#sets database to be already made photo_gallery, using PG.connect
 #
 get "/" do
   galleries = database.exec_params("SELECT name FROM galleries")
@@ -24,16 +24,26 @@ get "/" do
   erb :home #sinatra knows that method ERB calling :home will go to VIEWS/home.erb
 end
 
-get "/galleries/:name" do #:name here is part of a string, it is not a symbol
-  @name = params[:name]  
-  @images = GALLERIES[@name]
+#get "/galleries/:name" do #:name here is part of a string, it is not a symbol
+#
+get "/galleries/:id" do   
+  id = params[:id]   #setting id to parameter input in URL   galleries/1   id = 1
+ 
+  name_result = database.exec_params("SELECT name FROM galleries WHERE id = $1", [id])
+  #SELECT returns an array-like object from PG including names from galleries
+  #with id = 1  
+  
+  @name = name_result.first["name"]
+  #save @name as an instance variable to be available in :gallery view, equal to
+  #the value of the galleries "name" (value = "Cats", "Dogs" etc)
+  
+  @images = []
+  #@images = database.exec_params("SELECT * FROM images WHERE id = $1", [id])
+  
   erb :gallery
 end
-#
-#
-#
-#
-#
+
+
 #et "/projects/:id" do
 #  pass project id as variable
 #  create as a view using :e views/FILENAME.erb
