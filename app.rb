6921ca16a35ba3114c55ccdc1a53d11980ae7916
Nathan Beck <nathan.beck10@gmail.com>
2014-09-25 10:31:10 -0400

    Begin gallery additions from user

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ end
 require "active_record"
 
 ActiveRecord::Base.establish_connection(
-  adapter: "postgresql",
+  adapter:  "postgresql",
   database: "photo_gallery"
 )
 
@@ -18,14 +18,24 @@ end
 
 get "/" do 
   @galleries = Gallery.all
-  #@gallery_names = galleries.map {|gallery| gallery.name}
   erb :home
+end
+
+get "/galleries/new" do
+  erb :new_gallery
+end
+
+post "/galleries" do   #our form method on galleries/new is POST
+  new_gallery_name = params[:gallery][:name]
+  Gallery.create(name: new_gallery_name) 
+
+  redirect to("/")
 end
 
 get "/galleries/:id" do   
   id = params[:id]   #setting id to parameter input in URL   galleries/1   id = 1
   
-  gallery = Gallery.find(id) #finds galleries based on ID
+  gallery = Gallery.find(id) #finds gallery based on ID, creates ojbect array
  
   @name = gallery.name
   @images = Image.where(gallery_id: id)   # use with second part in gallery
@@ -33,7 +43,36 @@ get "/galleries/:id" do
   erb :gallery
 end
 
-  #name_result = database.exec_params("SELECT name FROM galleries WHERE id = $1", [id])
+#THESE ARE CLASS NOTES---------------------------------------
+
+# RUBY SCOPE
+#    SINATRA SCOPE
+#       get "/galleries" do
+#       end   etc...
+#
+# class Gallery < AcitveRecord::Base (Gallery inherits
+#    Gallery is the class that depends on the pluralization of Gallery in the
+#    database. In this instance the Gallery class will be full of stuff from
+#    galleries table in database. Active record does this automatically.
+#
+# Image.name will not work
+# image = Image.find(1) represents a row of stuff found in the images table
+# within the database.   .find  FINDS an instance (a row) of image class
+# image.name will now return the name of the name column from the images table
+#
+#@instance variables from within GET DO END (routes) cannot be tossed to other
+#routes. They are islands of code that are called and then tossed away.
+#A ROUTE is a combination of a VERB (get, post, patch...) and a PATH.
+
+#ORM Object Relational Mapper -- a bridge between RUBY code and database
+#   Dont have to write in SQL
+#   Takes ruby request, translates to database language
+#
+#if user is inputting something, always be wary of security risks
+#
+#@gallery_names = galleries.map {|gallery| gallery.name} gets array of names
+
+#name_result = database.exec_params("SELECT name FROM galleries WHERE id = $1", [id])
   #SELECT returns an array-like object from PG including names from galleries
   #with id = 1  
   
